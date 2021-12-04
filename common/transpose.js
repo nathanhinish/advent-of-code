cb1fa0313014997249a7435a38023edef6071b56
Nathan Hinish <nathan@triadinteractive.com>
2021-12-03 22:11:13 -0800

    Add a comment

--- common/transpose.js
+++ common/transpose.js
@@ -1,3 +1,5 @@
+// https://web.archive.org/web/20160725092857/http://www.codesuck.com/2012/02/transpose-javascript-array-in-one-line.html
+
 module.exports = function transpose(a) {
   return Object.keys(a[0]).map(function (c) {
     return a.map(function (r) {
