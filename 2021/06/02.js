5f4df119117b76270c56b92d3a9e9997bba4a366
Nathan Hinish <nathan@triadinteractive.com>
2021-12-06 13:48:15 -0800

    Quick cleanup

--- 2021/06/02.js
+++ 2021/06/02.js
@@ -7,28 +7,20 @@ const lines = require("../common/getLinesOfFile")("./input");
 let fishStates = lines[0].split(",").map((f) => parseInt(f, 10));
 
 // Initialize the array
-let groupedByState = fishStates.reduce((acc, v) => {
-  acc[v] = (acc[v] || 0) + 1;
-  return acc;
-}, []);
-
-// Fill in missing values
-for (let i = 0; i < 7; i++) {
-  if (groupedByState[i] === undefined) {
-    groupedByState[i] = 0;
-  }
-}
+let groupedByState = fishStates.reduce(
+  (acc, v) => {
+    acc[v] = (acc[v] || 0) + 1;
+    return acc;
+  },
+  [0, 0, 0, 0, 0, 0, 0, 0]
+);
 
 for (let dayNum = 1; dayNum <= 256; dayNum++) {
   const firstEl = groupedByState.shift();
 
   const numNewFish = firstEl;
 
-  if (groupedByState[6] === undefined) {
-    groupedByState[6] = firstEl;
-  } else {
-    groupedByState[6] = groupedByState[6] + firstEl;
-  }
+  groupedByState[6] = (groupedByState[6] || 0) + firstEl;
 
   if (groupedByState[7] === undefined) {
     groupedByState[7] = 0;
