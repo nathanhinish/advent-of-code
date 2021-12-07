64e6f8e1817c8ea17c39e476e8520fc06a59d303
Nathan Hinish <nathan@triadinteractive.com>
2021-12-06 21:06:09 -0800

    day 7 - part 1

--- 2021/07/01.js
+++ 2021/07/01.js
@@ -3,8 +3,23 @@ process.chdir(__dirname);
 let answer;
 
 const lines = require("../common/getLinesOfFile")("./input");
+const positions = lines[0].split(",").map((v) => parseInt(v, 10));
 
-// Add code here
+const minPos = Math.min(...positions);
+const maxPos = Math.max(...positions);
 
+let leastDiff = Number.POSITIVE_INFINITY;
+let bestPos = -1;
+
+for (let i = minPos; i <= maxPos; i++) {
+  const diff = positions.reduce((acc, v) => {
+    return acc + Math.abs(v - i);
+  }, 0);
+  if (diff < leastDiff) {
+    leastDiff = diff;
+    bestPos = i;
+  }
+}
+answer = leastDiff
 console.info("Answer:", answer);
 console.timeEnd("Run time");
