661bb24f1a600fc14a7c95a2c22f7682fd32451d
Nathan Hinish <nathan@triadinteractive.com>
2021-12-06 21:33:21 -0800

    day 7 - part 2

--- 2021/07/02.js
+++ 2021/07/02.js
@@ -3,8 +3,29 @@ process.chdir(__dirname);
 let answer;
 
 const lines = require("../common/getLinesOfFile")("./input");
+const positions = lines[0].split(",").map((v) => parseInt(v, 10));
 
-// Add code here
+const minPos = Math.min(...positions);
+const maxPos = Math.max(...positions);
 
+let leastDiff = Number.POSITIVE_INFINITY;
+
+const fibs = [0, 1];
+for (let i = 2; i <= maxPos; i++) {
+  fibs[i] = i + fibs[i - 1];
+}
+
+for (let i = minPos; i <= maxPos; i++) {
+  const diffs = positions.map((v) => Math.abs(v - i));
+  const costs = diffs.map((v) => fibs[v], 0);
+
+  const diff = costs.reduce((acc, v) => acc + v, 0);
+  if (diff < leastDiff) {
+    leastDiff = diff;
+    bestPos = i;
+  }
+}
+
+answer = leastDiff;
 console.info("Answer:", answer);
 console.timeEnd("Run time");
