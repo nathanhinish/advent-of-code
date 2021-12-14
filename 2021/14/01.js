98780cd64bb595ea1c421751b3361986d1d6296b
Nathan Hinish <nathan@triadinteractive.com>
2021-12-13 21:20:13 -0800

    day 14 - part 1

--- 2021/14/01.js
+++ 2021/14/01.js
@@ -4,16 +4,66 @@ let answer;
 
 const inputFile = process.argv[2];
 
-const lines = require("../common/getLinesOfFile")(inputFile).map(
-  require("../common/map_splitAndParse")
+const lines = require("../common/getLinesOfFile")(inputFile);
+
+const template = lines[0];
+const { keysToSearchFor, valuesToReplaceWith } = lines.slice(2).reduce(
+  ({ keysToSearchFor, valuesToReplaceWith }, l) => {
+    const parts = l.split(" -> ");
+    const pair = parts[0];
+    const replacement = pair[0] + parts[1] + pair[1];
+
+    return {
+      keysToSearchFor: [...keysToSearchFor, pair],
+      valuesToReplaceWith: [...valuesToReplaceWith, replacement],
+    };
+  },
+  { keysToSearchFor: [], valuesToReplaceWith: [] }
 );
 
-for (let y = 0; y < lines.length; y++) {
-  const line = lines[y];
-  for (let x = 0; x < line.length; x++) {
-    // Add code here
-  }
+const numSteps = 10;
+
+let currentState = template;
+for (let i = 0; i < numSteps; i++) {
+  let pairs = currentState.split("").reduce((acc, v, i, arr) => {
+    if (i > 0) {
+      acc.push(arr[i - 1] + arr[i]);
+    }
+    return acc;
+  }, []);
+
+  pairs = pairs.map((chars) => {
+    const index = keysToSearchFor.indexOf(chars);
+    if (index > -1) {
+      return valuesToReplaceWith[index];
+    } else {
+      return chars;
+    }
+  });
+
+  currentState = pairs.reduce((acc, v, i) => {
+    if (i > 0) {
+      acc += v.substr(1);
+    } else {
+      acc += v;
+    }
+    return acc;
+  }, "");
 }
+
+const charCounts = currentState.split("").reduce((acc, v) => {
+  if (acc[v] === undefined) {
+    acc[v] = 1;
+  } else {
+    acc[v] = acc[v] + 1;
+  }
+  return acc;
+}, {});
+const counts = Object.keys(charCounts).map((k) => charCounts[k]);
+const maxCount = Math.max(...counts);
+const minCount = Math.min(...counts);
+
+answer = maxCount - minCount;
 
 console.info("Answer:", answer);
 console.timeEnd("Run time");
