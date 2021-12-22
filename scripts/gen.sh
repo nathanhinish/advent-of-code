6bca65a1e033aadf6d45c9277b5ac55371f9ed07
Nathan Hinish <nathan@triadinteractive.com>
2021-12-22 15:33:51 -0800

    Retrieve input directly from AOC site

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+source .env.local
+
+url="https://adventofcode.com/2021/day/$1/input"
+cookie_header="Cookie: session=$AOC_SESSION_COOKIE"
+
 mkdir -p 2021/$1
 
-touch 2021/$1/input
+wget --header="$cookie_header" -O 2021/$1/input "$url" || echo "Fill with input data" > 2021/$1/input
 touch 2021/$1/sample
 cp scripts/templates/code.js 2021/$1/01.js
 cp scripts/templates/code.js 2021/$1/02.js
