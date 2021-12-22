85abd49b6f5be9e87485463ff9e69cf902e79907
Nathan Hinish <nathan@triadinteractive.com>
2021-12-22 11:37:47 -0800

    Use new template

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -6,3 +6,4 @@ touch 2021/$1/input
 touch 2021/$1/sample
 cp scripts/templates/code.js 2021/$1/01.js
 cp scripts/templates/code.js 2021/$1/02.js
+cp scripts/templates/parse.js 2021/$1/parse.js
--- scripts/templates/code.js
+++ scripts/templates/code.js
@@ -1,19 +1,39 @@
-console.time("Run time");
+console.time("Total run time");
 
-let answer;
+console.time("Init run time");
+const assert = require("assert");
+const parse = require("./parse");
 
-const inputFile = process.argv[2];
-
-const lines = require("../common/getLinesOfFile")(inputFile).map(
-  require("../common/map_splitAndParse")
+const TEST_FILES = ["sample"];
+const TEST_ANSWERS = {
+  sample: 0,
+};
+const TEST_DATA_SETS = TEST_FILES.reduce(
+  (o, f) => ({
+    ...o,
+    [f]: parse(f),
+  }),
+  {}
 );
+const INPUT_DATA_SET = parse("input");
+console.timeEnd("Init run time");
 
-for (let y = 0; y < lines.length; y++) {
-  const line = lines[y];
-  for (let x = 0; x < line.length; x++) {
-    // Add code here
-  }
+function run(key, data) {
+  console.info(`Running with '${key}' dataset`);
+  // Add code here
+  return 0;
 }
 
-console.info("Answer:", answer);
-console.timeEnd("Run time");
+console.time("Sample test run time time");
+TEST_FILES.forEach((key) => {
+  const result = run(key, TEST_DATA_SETS[key]);
+  assert.equal(result, TEST_ANSWERS[key]);
+});
+console.timeEnd("Sample test run time time");
+
+console.time("Answer run time");
+const result = run("input", INPUT_DATA_SET);
+console.info("ANSWER:", result);
+console.timeEnd("Answer run time");
+
+console.timeEnd("Total run time");
